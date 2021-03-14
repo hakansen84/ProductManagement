ab8be6260dc6e635ad722edab41378e15c0860d3
hakansen <hakansen84@gmail.com>
2021-03-15 00:22:13 +0300

    first commit

---
+++ src/labs/pm/data/Product.java
@@ -0,0 +1,67 @@
+package labs.pm.data;
+
+import java.math.BigDecimal;
+
+import static java.math.RoundingMode.HALF_UP;
+import static labs.pm.data.Rating.NOT_RATED;
+
+public class Product {
+
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+    private int id;
+    private String name;
+    private BigDecimal price;
+    private Rating rating;
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
+
+    public Product() {
+        this (0,"no name",BigDecimal.valueOf(0));
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+//    public void setId(final int id) {
+//        this.id = id;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public void setName(final String name) {
+//        this.name = name;
+//    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+//    public void setPrice(final BigDecimal price) {
+//        this.price = price;
+//    }
+
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public Product applyRating(Rating newRating){
+        return new Product(this.id,this.name,this.price,newRating);
+    }
+}
